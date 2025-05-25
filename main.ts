4bf6a74e36e7db4d61c3357c955f624d66519d86
microboy786 <kavithasupramanium@gmail.com>
2025-05-25 00:02:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showString("HOW ARE YOU")
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showString("HELLO")
+})
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showString("HI")
+})
